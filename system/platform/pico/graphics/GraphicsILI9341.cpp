9b943b50eac43a15989657907ccfe59505497c10
mshu <59457622+mshujp@users.noreply.github.com>
2026-08-11 18:52:34 +0900

    Update GraphicsILI9341.cpp

--- system/platform/pico/graphics/GraphicsILI9341.cpp
+++ system/platform/pico/graphics/GraphicsILI9341.cpp
@@ -446,7 +446,7 @@ void GraphicsILI9341::getClipRect(int16_t& x, int16_t& y, uint16_t& w, uint16_t&
     int32_t cy;
     int32_t cw;
     int32_t ch;
-    canvas.getClipRect(&cx, &cx, &cw, &ch);
+    canvas.getClipRect(&cx, &cy, &cw, &ch);
     x = static_cast<int16_t>(cx);
     y = static_cast<int16_t>(cy);
     w = static_cast<int16_t>(cw);
